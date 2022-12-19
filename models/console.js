7f1d9dbb9bf4500303605445b8c58c7950fb2591
Ben <benawhiting@gmail.com>
2022-12-19 13:49:32 -0800

    Create schema for consoles

---
+++ models/console.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const Schema = mongoose.Schema;
+
+const ConsoleSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  about: { type: String, required: false, maxLength: 1000 },
+  release: { type: Date },
+});
+
+// Virtual for console's URL
+ConsoleSchema.virtual('url').get(function() {
+  return `/catalog/consoles/${this._id}`;
+});
+
+// Virtual for formatted release date of console
+ConsoleSchema.virtual('release_formatted').get(function() {
+  return DateTime.fromJSDate(this.release).toLocaleString(DateTime.DATE_MED);
+});
+
+module.exports = mongoose.model('Console', ConsoleSchema);
