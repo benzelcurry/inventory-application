776a392d30153ae8804b4c686d23962a0e38cab8
Ben <benawhiting@gmail.com>
2022-12-21 12:33:08 -0800

    Create details page for individual studios

--- controllers/studioController.js
+++ controllers/studioController.js
@@ -2,6 +2,7 @@ const Studio = require('../models/studio');
 const Game = require('../models/game');
 
 const async = require('async');
+const studio = require('../models/studio');
 
 // Display list of all Studios
 exports.studio_list = function (req, res, next) {
@@ -17,4 +18,36 @@ exports.studio_list = function (req, res, next) {
         studio_list: list_studios,
       });
     });
+}
+
+// Display detail page for a specific studio
+exports.studio_detail = (req, res, next) => {
+  async.parallel(
+    {
+      studio(callback) {
+        Studio.findById(req.params.id).exec(callback);
+      },
+      // Might need to tweak this to work; wrote down field names by memory
+      studio_games(callback) {
+        Game.find({ studio: req.params.id }, "name about").exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        // Error in API usage
+        return next(err);
+      }
+      if (results.studio == null) {
+        // No results
+        const err = new Error('Author not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render('studio_detail', {
+        studio: results.studio,
+        games: results.studio_games,
+      });
+    }
+  );
 }
